2a329eea92c8c04bb66eef3062dd4fe6c112352a
Jorge Parra <jorgemparrah@gmail.com>
2025-10-14 21:30:49 -0300

    Ejemplos - Crear BD y Tablas

---
+++ libreria.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE libreria2;
+
+USE libreria2;
+
+CREATE TABLE editorial(
+	id INTEGER PRIMARY KEY,
+	nombre VARCHAR(50),
+	nombre_contacto VARCHAR(50),
+	direccion VARCHAR(100),
+	ciudad VARCHAR(30),
+	telefono VARCHAR(12),
+	email VARCHAR(30),
+	comentario VARCHAR(500)
+);
+
+CREATE TABLE libro(
+	id INTEGER PRIMARY KEY,
+	isbn VARCHAR(17),
+	titulo VARCHAR(50),
+	id_editorial INTEGER,
+	anno YEAR,
+	precio INTEGER,
+	comentarios VARCHAR(500),
+	foto BLOB,
+  FOREIGN KEY (id_editorial) REFERENCES editorial(id)
+);
+
